01f2b31b5aa917107bfba4d35e9d5b350bfcffcb
Wexoritta <svetlanawex@gmail.com>
2019-12-23 09:11:59 +0400

    enter

--- assets/js/app.js
+++ assets/js/app.js
@@ -52,10 +52,25 @@ $(function () {
      });   
 
 
+     /*  Collapse  */
+
+     $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+        blockId = $this.data('collapse');
+
+        $this.toggleClass("active");
+     });
 
 
-
-
+     /* Slider */
+     $("[data-slider]").slick({
+        infinite: true,
+        fade: false,
+        slideToShow: 1,
+        slidesToScroll: 1
+     });
 
 
 });
